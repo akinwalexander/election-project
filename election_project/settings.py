0bd02f56add3695ce0d035e142e22f1345df4baf
akinwalexander <alexanderakinlami4@gmail.com>
2025-04-24 13:37:27 +0100

    Update ALLOWED_HOSTS for Render

--- election_project/settings.py
+++ election_project/settings.py
@@ -29,7 +29,11 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'election-project.onrender.com',  # Render domain
+    'localhost',  # For local testing
+    '127.0.0.1',   # For local testing
+]
 
 
 # Application definition
